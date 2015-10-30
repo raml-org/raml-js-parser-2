a45672f857e0419c4337d406332ad95e74f6c663
Konstantin Sviridov <ksviridov@gmail.com>
2015-10-30 08:54:20 +0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,10 +2,12 @@ console.log('RAML 1.0 JS Parser Test');
 
 require('../src/bundle');
 
+var RAML = global.RAML;
+
 var path = require("path");
 
-var api = global.RAML.loadApi(path.resolve(__dirname, "../raml-specs/XKCD/api.raml")).getOrElse(null);
+var api = RAML.loadApi(path.resolve(__dirname, "../raml-specs/XKCD/api.raml")).getOrElse(null);
 
 console.log( "Some method name: " + api.resources()[0].methods()[0].method() );
 
-console.log(console.log(JSON.stringify(global.RAML.toJSON(api), null, 2)));
+console.log(console.log(JSON.stringify(RAML.toJSON(api), null, 2)));
