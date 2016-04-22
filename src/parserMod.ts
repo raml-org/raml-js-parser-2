541bfe87f4f698bd890e53b2b549b478d9d6478c
KonstantinSviridov <ksviridov@gmail.com>
2016-04-22 20:44:17 +0500

    setTypeExample parserMod method

--- src/parserMod.ts
+++ src/parserMod.ts
@@ -5,6 +5,9 @@ import coreImpl=require("./raml1/wrapped-ast/parserCore");
 import highLevel = require("./raml1/highLevelAST");
 import stubs = require("./raml1/stubs");
 import defSys = require("raml-definition-system")
+import jsyaml=require("./raml1/jsyaml/jsyaml2lowLevel")
+import ll=require("./raml1/lowLevelAST")
+import {universeHelpers} from "../dist/index";
 
 
 export function createTypeDeclaration(typeName : string) : parser10api.TypeDeclaration {
@@ -21,12 +24,18 @@ export function setTypeDeclarationSchema(type: parser10api.TypeDeclaration, sche
 }
 
 export function setTypeDeclarationExample(type: parser10api.TypeDeclaration, example : string) {
-    // var exampleSpecType = defSys.getUniverse("RAML10").type(defSys.universesInfo.Universe10.ExampleSpec.name);
-    // var examplePropName = defSys.universesInfo.Universe10.TypeDeclaration.properties.example.name;
-    // var stubNode = stubs.createStubNode(exampleSpecType,type.definition().property(examplePropName));
-    // (<parser10impl.ExampleSpecImpl>stubNode.wrapperNode()).setValue(example);
-    // type.highLevel().add(stubNode);
-    
+    var exampleSpecType = defSys.getUniverse("RAML10").type(defSys.universesInfo.Universe10.ExampleSpec.name);
+    var examplePropName = defSys.universesInfo.Universe10.TypeDeclaration.properties.example.name;
+    var hlParent = type.highLevel();
+    var llParent = hlParent.lowLevel();
+    var exampleNodes = hlParent.children().filter(x=>x.lowLevel().key()==examplePropName);
+    var llNode = jsyaml.createNode(examplePropName);
+    ll.setAttr(llNode,example);
+    if(exampleNodes.length>0){
+        ll.removeNode(llParent, exampleNodes[0].lowLevel());
+        (<any>exampleNodes[0])._node = llNode;
+    }
+    ll.insertNode(llParent,llNode);
 }
 
 export function addChild(parent : highLevel.BasicNode, child : highLevel.BasicNode) : void {
