2a96648a6770b0ed39d28b6c82a8d5240dad3d62
KonstantinSviridov <ksviridov@gmail.com>
2016-07-11 20:38:40 +0500

    Fixing https://github.com/raml-org/raml-js-parser-2/issues/234

--- src/raml1/test/parserTests.ts
+++ src/raml1/test/parserTests.ts
@@ -1160,7 +1160,7 @@ describe('Property override tests',function(){
         testErrors(util.data("parser/custom/missedTitle.raml"),["property 'title' must be a string"]);
     });
     it ("expander not halted by this sample any more",function(){
-        testErrorsByNumber(util.data("parser/custom/expanderHalt.raml"),14);
+        testErrorsByNumber(util.data("parser/custom/expanderHalt.raml"),10);
     });
 });
 describe('Line mapper tests',function() {
