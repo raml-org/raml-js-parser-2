f654e6087fd65c1bddb28927522713196776b5a2
Denis Denisenko <denis@onpositive.com>
2016-10-07 11:31:29 +0700

    https://github.com/raml-org/raml-js-parser-2/issues/513 fixed.

--- src/raml1/test/parserTests2.ts
+++ src/raml1/test/parserTests2.ts
@@ -825,8 +825,12 @@ describe('Modularization', function(){
         assert.equal(serializedJSON.indexOf("required") > 0, true)
     })
 
-    it('Should parse overlay',function(){
+    it('Should not expode on empty extension',function(){
         testErrors(util.data('extensions/empty.raml'),["Missing required property: 'extends'"]);
+    });
+
+    it('Should translate errors from invalid api to extension',function(){
+        testErrors(util.data('extensions/invalidApiExtension.raml'),["Unknown node: 'unknown'"]);
     });
 });
 
