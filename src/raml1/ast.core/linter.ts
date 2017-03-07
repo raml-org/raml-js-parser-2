685c53c83fa13d99ed41c3af655cc69a97b852fd
KonstantinSviridov <ksviridov@gmail.com>
2017-03-07 21:11:57 +0600

    reporting error in JSON schemas as warnings wor RAML 0.8

--- src/raml1/ast.core/linter.ts
+++ src/raml1/ast.core/linter.ts
@@ -1464,6 +1464,7 @@ function isValidValueType(t:hl.ITypeDefinition,h:hl.IHighLevelNode, v:any,p:hl.I
                         tm.validateSelf();
                     }
                     catch(e){
+                        e['isWarning'] = true;
                         return e;
                     }
                 }
@@ -1551,10 +1552,10 @@ class NormalValidator implements PropertyValidator{
             if (!(<any>validation).canBeRef){
                 if(ValidationError.isInstance(validation)){
                     var ve = <ValidationError>validation;
-                    v.accept(createIssue1(ve.messageEntry,ve.parameters, node));
+                    v.accept(createIssue1(ve.messageEntry,ve.parameters, node, (<any>validation).isWarning));
                 }
                 else {
-                    v.accept(createIssue1(messageRegistry.SCHEMA_EXCEPTION, {msg: (<Error>validation).message}, node));
+                    v.accept(createIssue1(messageRegistry.SCHEMA_EXCEPTION, {msg: (<Error>validation).message}, node,(<any>validation).isWarning));
                 }
                 validation = null;
                 return;
@@ -2233,10 +2234,10 @@ class DescriminatorOrReferenceValidator implements PropertyValidator{
             if (validation instanceof Error) {
                 if(ValidationError.isInstance(validation)){
                     var ve = <ValidationError>validation;
-                    cb.accept(createIssue1(ve.messageEntry,ve.parameters, node));
+                    cb.accept(createIssue1(ve.messageEntry,ve.parameters, node,validation.isWarning));
                 }
                 else {
-                    cb.accept(createIssue1(messageRegistry.SCHEMA_EXCEPTION, {msg:(<Error>validation).message}, node));
+                    cb.accept(createIssue1(messageRegistry.SCHEMA_EXCEPTION, {msg:(<Error>validation).message}, node,validation.isWarning));
                 }
                 validation = null;
             }
