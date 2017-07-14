f8cd2a48bff4172933426d38322f2c9b3537e06a
KonstantinSviridov <ksviridov@gmail.com>
2017-07-14 23:28:00 +0600

    Comments for HasSources.scalarFields and Resource.(baseUri,completeRelativeUri,parentUri and absoluteParentUri)

--- src/typings-new-format/spec-1.0/common.ts
+++ src/typings-new-format/spec-1.0/common.ts
@@ -13,7 +13,11 @@ export interface SourceInfo{
 }
 
 export interface HasSource extends SourceInfo{
-
+	
+	/**
+	 * Source information for fields which are defined in another file rather then their owning component.
+     * If all scalar fields of the component are defined in the same file, the 'scalarsSources' field is undefined.
+	 */
     scalarsSources?: { [key:string]:SourceInfo[] }
 
     __METADATA__?: any
